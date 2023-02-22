c6d2a64af369b80913158355c18525174b3b3e6c
S Krishna Sneha <87603454+Krishna-Sneha@users.noreply.github.com>
2023-02-22 16:29:16 +0530

    Update GetKeypadCombination.cpp

--- GetKeypadCombination.cpp
+++ GetKeypadCombination.cpp
@@ -1,3 +1,16 @@
+// 1. You are given a string str. The string str will contains numbers only, where each number stands for a key pressed on a mobile phone.
+// 2. The following list is the key to characters map :
+//     0 -> .;
+//     1 -> abc
+//     2 -> def
+//     3 -> ghi
+//     4 -> jkl
+//     5 -> mno
+//     6 -> pqrs
+//     7 -> tu
+//     8 -> vwx
+//     9 -> yz
+
 #include<iostream>
 #include<string>
 #include<vector>
